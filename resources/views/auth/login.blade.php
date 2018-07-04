471c674b6800fe3cd7761d14f6332ce945ec646a
TomRod7 <tom.rodriguez@live.com.ar>
2018-07-04 19:24:21 -0300

    Logueo arreglado.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,5 +45,20 @@
     </div>
   </div>
 
-@include('includes.footer')
+<footer class="footer">
+  <div style="padding-top: 62px;"class="container">
+    <nav class="pull-left">
+      <ul>
+        <li class="color"><a href="{{ ('/') }}">Home</a></li>
+        <li class="color"><a href="{{ ('/quienes') }}">Quienes Somos</a></li>
+        <li class="color"><a href="{{ ('/products') }}">Productos</a></li>
+        <li class="color"><a href="{{ ('/contacto') }}">Contacto</a></li>
+      </ul>
+    </nav>
+    <div class="copyright pull-right" style="color: #f3c94b;">
+      Tomás Rodriguez &copy; 2018
+    </div>
+  </div>
+  <img class="imagenfoo" src="{{ asset ('/img/inferior.png') }}">
+</footer>
 @endsection
